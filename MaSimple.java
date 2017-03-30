073b7817f3961746a25eeaf550fbd262394b40ae
Eric Nyandwi <ericnyandwi_803@hotmail.com>
2017-03-30 17:32:49 +0200

    Mon premier commit

---
+++ MaSimple.java
@@ -0,0 +1,21 @@
+public class MaSimple {
+
+	 public static void main(String[] argv){
+  (new A()).saluer();
+    }
+}
+
+class A{
+    B b = new B();
+    void saluer(){
+        System.out.println("Bonjour, je suis une instance de A!");
+  System.out.println("Je vais appeler une instance de B!");
+  b.saluer();
+    }
+}
+
+class B{
+    void saluer(){
+        System.out.println("Bonjour, je suis une instance de B!");
+    }
+}
